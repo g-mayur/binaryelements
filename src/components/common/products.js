6a424a3d2db51b5a7c8c7f8062b1c6fe0189fa85
YashKhalasi <yash.k@crestinfosystems.com>
2022-10-07 11:53:48 +0530

    Product Slider Changes

--- src/components/common/products.js
+++ src/components/common/products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import Slider from 'react-slick';
 import Communicate from '../../assets/products/communicate.png';
 import Run from '../../assets/products/run.png';
@@ -9,181 +10,131 @@ import Automate from '../../assets/products/automate.png';
 import Future from '../../assets/products/future.png';
 import { Link } from 'gatsby';
 import { Col, Container, Row } from 'react-bootstrap';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import VoipElements from ""
 
 const Products = () => {
-const [slider,setSlider] = useState()
-const [slideIndex,setSlideIndex] = useState()
-const [tempYScroll,setTempYScroll] = useState(0);
-const [tempXScroll,setTempXScroll] = useState(0);
 
-   const  scroll = (y) =>{
-        //console.log(y);
-        y > 0 ? (
-           slider?.slickPrev()
-        ) : (
-            slider?.slickNext()
-        )
-    }
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect");
+        gsap.registerPlugin(ScrollTrigger);
 
-    const changeXScroll = (slide) => {
-      setTempXScroll(1);
-    }
+        const pinBoxes = document.querySelectorAll(".pin-wrap > *");
+        const pinWrap = document.querySelector(".pin-wrap");
 
-    const changeYScroll = (slide) => {
-      setTempYScroll(1);
-    }
+        let pinWrapWidth;
+        let horizontalScrollLength;
 
-    useEffect(() => {
-      if (tempXScroll == 1) {
-        setTimeout(() => {
-          window.scroll({
-            top: document.getElementById("section_2").offsetTop,
-            behavior: 'smooth'
+        function refresh() {
+          pinWrapWidth = pinWrap.scrollWidth;
+          horizontalScrollLength = pinWrapWidth - window.innerWidth;
+          //console.log(pinWrapWidth);
+        }
+
+        if (document.readyState === "complete") {
+          //do nothing
+        } else {
+          
+        window.addEventListener("load", function () {
+          console.log("window load listener");
+          refresh();
+          gsap.registerEase("myEaseName", function(progress) {
+            return progress; //linear
           });
-          setTempXScroll(0);
-        }, 100);
-      }
-    }, [tempXScroll]);
+            // Pinning and horizontal scrolling
+            gsap.to(".pin-wrap", {
+              scrollTrigger: {
+                scrub: 1,
+                trigger: "#sectionPin",
+                pin: true,
+                pinSpacer: ".pin-spacer",
+                pinSpacing: true,
+                start: "center center",
+                markers: true,
+                onEnter: () => console.log("enter"),
+                end: () => `+=${pinWrapWidth}`,
+                invalidateOnRefresh: true,
+              },
+              duration: 5,
+              x: () => -horizontalScrollLength - 100,
+              ease:"none"
+            });
 
-    useEffect(() => {
-      if (tempYScroll == 1) {
-        setTimeout(() => {
-          window.scroll({
-            top: document.getElementById("section_3").offsetTop - 300,
-            behavior: 'smooth'
+            ScrollTrigger.addEventListener("refreshInit", refresh);
           });
-          setTempYScroll(0);
-        }, 100);
-      }
-    }, [tempYScroll]);
+        }
 
-    useEffect(() => {
-        const ProductWrapper = document.querySelector(".product-slider");
-        ProductWrapper.addEventListener('wheel', (e) => {
-            scroll(e.wheelDelta);
-            e.preventDefault();
-        })
-    },)
+        /* // Next button
+        function slideOneNext() {
+          gsap.fromTo(targets[count], { xPercent: 0, zIndex: 0 }, { delay: 0.2, duration: 1.2, xPercent: 0, zIndex: -10 });
+          count = count < targets.length - 1 ? ++count : 0;
+          gsap.fromTo(targets[count], { xPercent: 100, zIndex: 10 }, { duration: 1.2, xPercent: 0, zIndex: 0 });
+        }
+        nextButton.addEventListener("click", function() {
+          slideOneNext();
+        });
+      
+        // Previous button
+        function slideOnePrev() {
+          gsap.fromTo(targets[count], { xPercent: 0, zIndex: 10 }, {xPercent: 0, zIndex: 0 });
+          gsap.fromTo(targets[count], { xPercent: 0, zIndex: 0 }, { delay: 0, duration: 1.2, xPercent: 100, zIndex: -10 });
+          count = count < targets.length ? --count : 0;
+        }
+        prevButton.addEventListener("click", function() {
+          slideOnePrev();
+        }); */
+      }, [])
 
-    var settings = {
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3.5,
-        slidesToScroll: 1,
-        centerPadding: "60px",
-        afterChange: function(index) {
-          if ((index + 1) >= 2.5) {
-            changeYScroll(1);
-          }
-          if ((index + 1) <= 1) {
-            changeXScroll(1);
-          }
-        },
-        responsive: [
-          {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 1.5,
-                slidesToScroll: 1,
-            }
-            },
-            {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1.5,
-                slidesToScroll: 1,
-            }
-            },
-            {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2,
-                settings: 'unslick'
-            }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 0,
-                vertical: true,
-                verticalSwiping: true,
-                beforeChange: function(currentSlide, nextSlide) {
-                  //console.log("before change", currentSlide, nextSlide);
-                },
-                afterChange: function(currentSlide) {
-                  //console.log("after change", currentSlide);
-                }
-            }
-            }
-        ]
-        };
+    
   return (
     <>
-    <div className='mt-5 product-slider d-none d-md-block'>
-        <Slider {...settings} ref={slider => setSlider(slider)}>
-          <div className='product__wrapper py-5 communicate'>
-            <Link to="/"><img src={Communicate} alt="Communicate" className='mx-auto' /></Link>
-            <h3 className='product_title mt-5 mb-3'>Communicate — <span>VOIPElements</span></h3>
-            <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
-          </div>
-          <div className='product__wrapper py-5 run'>
-            <Link to="/"><img src={Run} alt="Run" className='mx-auto' /></Link>
-            <h3 className='product_title mt-5 mb-3'>Run — <span>ITElements</span></h3>
-            <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
-          </div>
-          <div className='product__wrapper py-5 protect'>
-            <Link to="/"><img src={Protect} alt="Protect" className='mx-auto' /></Link>
-            <h3 className='product_title mt-5 mb-3'>Protect — <span>CloudElements</span></h3>
-            <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
-          </div>
-          <div className='product__wrapper py-5 automate'>
-            <Link to="/"><img src={Automate} alt="Automate" className='mx-auto' /></Link>
-            <h3 className='product_title mt-5 mb-3'>Autommate — <span>GrowthElements</span></h3>
-            <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
-          </div>
-          <div className='product__wrapper py-5 future'>
-            <Link to="/"><img src={Future} alt="Future" className='mx-auto' /></Link>
-            <h3 className='product_title mt-5 mb-3'>Future — <span>IoTElements</span></h3>
-            <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
-          </div>
-        </Slider>
-    </div>
-    <Container fluid>
-        <Row className='justify-content-center'>
-          <Col xs={10} sm={10} md={10} lg={10} xl={8}>
-            <div className='mt-5 product-slider d-block d-md-none'>
-              <div className='product__wrapper py-5 communicate'>
-                <Link to="/"><img src={Communicate} alt="Communicate" className='mx-auto' /></Link>
-                <h3 className='product_title mt-5 mb-3'>Communicate — <span>VOIPElements</span></h3>
-                <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+        <div className="pin-spacer product-slider mt-5">
+          <div
+            id="sectionPin"
+          >
+            <div className="row d-flex flex-nowrap py-70 pin-wrap">
+              <div className="d-flex flex-wrap col-6 col-sm-6 col-md-2 process-item process-item--nobefore product__wrapper py-5 communicate">
+                  <div className="product-slide-inner-block">
+                    <Link to="/"><img src={Communicate} alt="Communicate" className='mx-auto' /></Link>
+                    <h3 className='product_title mt-5 mb-3'>Communicate — <span>VOIPElements</span></h3>
+                    <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                  </div>
               </div>
-            <div className='product__wrapper py-5 run'>
-                <Link to="/"><img src={Run} alt="Run" className='mx-auto' /></Link>
-                <h3 className='product_title mt-5 mb-3'>Run — <span>ITElements</span></h3>
-                <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+              <div className="d-flex flex-wrap col-12 col-sm-6 col-md-2 process-item product__wrapper py-5 run">
+                <div className="product-slide-inner-block">
+                    <Link to="/"><img src={Run} alt="Run" className='mx-auto' /></Link>
+                    <h3 className='product_title mt-5 mb-3'>Run — <span>ITElements</span></h3>
+                    <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                </div>
               </div>
-            <div className='product__wrapper py-5 protect'>
-                <Link to="/"><img src={Protect} alt="Protect" className='mx-auto' /></Link>
-                <h3 className='product_title mt-5 mb-3'>Protect — <span>CloudElements</span></h3>
-                <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+
+              <div className="d-flex flex-wrap col-12 col-sm-6 col-md-2 process-item product__wrapper py-5 protect">
+                <div className="product-slide-inner-block">
+                  <Link to="/"><img src={Protect} alt="Protect" className='mx-auto' /></Link>
+                  <h3 className='product_title mt-5 mb-3'>Protect — <span>CloudElements</span></h3>
+                  <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                </div>
               </div>
-            <div className='product__wrapper py-5 automate'>
-                <Link to="/"><img src={Automate} alt="Automate" className='mx-auto' /></Link>
-                <h3 className='product_title mt-5 mb-3'>Autommate — <span>GrowthElements</span></h3>
-                <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+
+              <div className="d-flex flex-wrap col-12 col-sm-6 col-md-2 process-item product__wrapper py-5 automate">
+                <div className="product-slide-inner-block">
+                  <Link to="/"><img src={Automate} alt="Automate" className='mx-auto' /></Link>
+                  <h3 className='product_title mt-5 mb-3'>Autommate — <span>GrowthElements</span></h3>
+                  <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                </div>
               </div>
-            <div className='product__wrapper py-5 future'>
-                <Link to="/"><img src={Future} alt="Future" className='mx-auto' /></Link>
-                <h3 className='product_title mt-5 mb-3'>Future — <span>IoTElements</span></h3>
-                <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+
+              <div className="d-flex flex-wrap col-12 col-sm-6 col-md-2 process-item product__wrapper py-5 future">
+                <div className="product-slide-inner-block"> 
+                  <Link to="/"><img src={Future} alt="Future" className='mx-auto' /></Link>
+                  <h3 className='product_title mt-5 mb-3'>Future — <span>IoTElements</span></h3>
+                  <p className='product__description'>Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                  </div>
               </div>
             </div>
-          </Col>
-        </Row>
-    </Container>
+          </div>
+      </div>
     </>
   )
 }
